1fe5280ce9afe463edd250c5ed76e9e378d14e2f
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2024-07-19 15:09:59 +0530

    updated file name

--- frosh/src/App.jsx
+++ frosh/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import OurTeam from '../src/components/OurTeam.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <OurTeam />
     </>
   )
 }
---
+++ frosh/src/components/OurTeam.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const OurTeam = () => {
+    return (
+        <div className='bg-ourTeam' id='about'>
+            <div className="frosh-alpha">
+                <div className="three">
+                    <div className="one" >F</div>   
+                    <div className="two" >FAMILY</div>
+                    <p>Frosh aims to build a sense of faith amongst the three pillars holding the institute together with the faculty, the seniors, and the freshers.</p>
+                </div>
+                <div className="three">
+                    <div className="one">R</div>   
+                    <div className="two" >RELIABLE</div>
+                    <p>Frosh provides a reliable support system to hold hands through times that one might face.</p>
+
+                </div >
+                <div className="three">
+            
+                    <div className="one" >O</div>   
+                    <div className="two">OPTIMISTIC</div>
+                    <p>Frosh believes in an optimistic approach while dealing with the challenges one has to face in life.</p>
+                
+                </div>
+                <div className="three">
+                    <div className="one" >S</div>   
+                    <div className="two">SPRITED</div>
+                    <p>Frosh creates an energetic and spirited environment for the freshers for them to comfortably express their talents.</p>
+                
+                </div>
+                <div className="three">
+                    <div className="one" >H</div>   
+                    <div className="two"  >HUMBLE</div>
+                    <p>Frosh lays out a humble atmosphere for the freshers to approach them without hesitation.</p>
+                
+                </div>
+            </div>
+            <div className="faculty-team">
+                <Link to="/faculty" style={{"textDecoration":"none"}}><h1 className="faculty">FACULTY</h1></Link>
+                <Link to="/team" style={{"textDecoration" : "none"}}><h1 className="team">TEAM</h1></Link>
+                
+                
+            </div>
+        </div>
+    )
+
+}
+
+export default OurTeam
