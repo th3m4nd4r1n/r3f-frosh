130544d1b450a26b6e18a9ab8ec82d4c6fbeca81
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-07-22 02:14:27 +0530

    added a compass replacement until i get the compass model from blender

--- frosh/src/App.jsx
+++ frosh/src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    
-    </>
+
+function Model() {
+  const gltf = useLoader(GLTFLoader, '../public/scene.gltf');
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.y=Math.PI;
+    ref.current.rotation.z=state.clock.elapsedTime;
+    ref.current.position.z=-1;
+ 
+  });
+
+  return <primitive object={gltf.scene} ref={ref} />;
+}
+const Compass = () =>{
+  return(
+    <Canvas>
+    <ambientLight intensity={40} />
+    <Model />
+    <OrbitControls enablePan={false} enableZoom={false} enableDamping={true} enableRotate={true} enabled={true} />
+    </Canvas>
   )
+}
+
+function App() {
+  return (
+    <div className='elements'>
+
+  <Compass />
+  
+  
+  
+    </div>
+
+
+    
+
+  );
 }
 
 export default App
